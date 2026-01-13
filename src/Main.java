e700b7ee48cfa0bbfa9c8c0c6376673a24c120a5
20253ds097-netizen <20253ds097@utez.edu.mx>
2026-01-12 23:40:08 -0600

    codigoholamundo

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+public class Main {
+    public static void main(String[] args) {
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+        String saludo = "Hola mundo";
+        String nombre = "Luis Uriel Vargas Espino";
+        String grupo  = "2D";
+        String fecha  = "12 / 01 / 2026";
+        System.out.println(" MENSAJE: " + saludo);
+        System.out.println(" NOMBRE:  " + nombre);
+        System.out.println(" GRUPO:   " + grupo);
+        System.out.println(" FECHA:   " + fecha);
+
     }
 }
