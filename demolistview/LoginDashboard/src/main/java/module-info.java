abdbdd4b811c9f2bf6be02910eefded954456a4a
20253ds097-netizen <20253ds097@utez.edu.mx>
2026-03-09 11:30:29 -0600

    update

--- demolistview/LoginDashboard/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.logindashboard {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.logindashboard to javafx.fxml;
-    exports com.example.logindashboard;
-}
